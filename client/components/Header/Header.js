cd8be53a48761d91ddcfa7e5a359b6d96bdbccde
Anukritik007 <anukritik77@gmail.com>
2022-04-16 20:43:52 +0530

    Added Menu drawer for mobile device

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Header.scss";
 import { Link, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -6,6 +6,7 @@ import AuthContext from "../../AuthContext";
 
 const Header = () => {
   const history = useHistory();
+  const [showDrawer, setShowDrawer] = useState(false);
   const { userAuthentication, toggleUserAuthentication } = useContext(
     AuthContext
   );
@@ -15,9 +16,39 @@ const Header = () => {
       state.cart && state.cart.itemsAdded && state.cart.itemsAdded.length
   );
 
+  const getAuthJSX = () => {
+    return userAuthentication === "logged-in" ? (
+      <li
+        onClick={() => {
+          sessionStorage.setItem("status", "");
+          toggleUserAuthentication();
+        }}
+      >
+        <a href="#">Logout</a>
+      </li>
+    ) : (
+      <>
+        <li>
+          <Link to="/login">SignIn</Link>
+        </li>
+        <li>
+          <Link to="/sign-up">Register</Link>
+        </li>
+      </>
+    );
+  };
+
   return (
-    <header className="header">
+    <header
+      className={`header ${showDrawer ? "expand-header" : "collapse-header"}`}
+    >
       <nav className="navbar">
+        <button
+          className="hamburger"
+          onClick={() => setShowDrawer(!showDrawer)}
+        >
+          <i className="fas fa-bars"></i>
+        </button>
         <div className="brand-name">
           <img
             className="brand-logo"
@@ -25,7 +56,6 @@ const Header = () => {
             alt="Sabka Bazaar Logo"
             height="60"
             width="100"
-            onClick={() => history.push("/")}
           />
         </div>
         <div className="left-nav">
@@ -39,28 +69,7 @@ const Header = () => {
           </ul>
         </div>
         <div className="right-nav">
-          {userAuthentication === "logged-in" ? (
-            <ul className="links">
-              <li
-                onClick={() => {
-                  sessionStorage.clear();
-                  sessionStorage.setItem("status", "");
-                  toggleUserAuthentication();
-                }}
-              >
-                <a href="#">Logout</a>
-              </li>
-            </ul>
-          ) : (
-            <ul className="links">
-              <li>
-                <Link to="/login">SignIn</Link>
-              </li>
-              <li>
-                <Link to="/sign-up">Register</Link>
-              </li>
-            </ul>
-          )}
+          <ul className="links">{getAuthJSX()}</ul>
           <div className="my-cart">
             <Link to="/cart">
               <img
@@ -80,6 +89,22 @@ const Header = () => {
             </Link>
           </div>
         </div>
+      </nav>
+      <nav className="menubar">
+        {showDrawer && (
+          <ul
+            className="hamburger-menu"
+            onClick={() => setShowDrawer(!showDrawer)}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            {getAuthJSX()}
+          </ul>
+        )}
       </nav>
     </header>
   );
